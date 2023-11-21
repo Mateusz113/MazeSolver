dde893557ab005847d32010344212989d83b4f9c
Mateusz113 <mateusz.ch.113@gmail.com>
2023-11-21 17:36:15 +0100

    Update MazeInfo.kt
    Fixed the typoo

--- app/src/main/java/uk/ac/aber/dcs/cs39440/maze_solver/util/enums/MazeInfo.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs39440/maze_solver/util/enums/MazeInfo.kt
@@ -16,6 +16,6 @@ enum class MazeInfo(val size: Pair<Int, Int>) {
 //Map of labels to display
 val mazeInfoLabelsMap = mapOf(
     MazeInfo.Small to R.string.twenty_by_thirty,
-    MazeInfo.Medium to R.string.thirty_by_fourtyfive,
-    MazeInfo.Large to R.string.fourty_by_sixty
+    MazeInfo.Medium to R.string.thirty_by_fortyfive,
+    MazeInfo.Large to R.string.forty_by_sixty
 )
